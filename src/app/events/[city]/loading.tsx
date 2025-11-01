c39738c2be7f8b9c19f5cf1f6c9771c91ce03e3f
Lilit <baregamyanlilit36@gmail.com>
2025-11-01 17:36:53 +0400

    Suspense 235

--- src/app/events/[city]/loading.tsx
+++ src/app/events/[city]/loading.tsx
@@ -2,7 +2,7 @@ import SkeletonCard from "@/components/skeleton-card";
 
 export default function Loading() {
   return (
-    <div className="flex felx-wrap justify-center max-w-[1100px] mx-auto px-5 py-24 gap-20">
+    <div className="flex flex-wrap justify-center max-w-[1100px] mx-auto px-5 py-24 gap-20">
       {Array.from({ length: 6 }).map((item, i) => (
         <SkeletonCard key={i} />
       ))}
